f6ff244f0664aab4d65e2a53147b7f19cff16aea
Arda Celik <ardacelik.dev@gmail.com>
2023-08-10 20:17:59 +0300

    style(Chat.js): removed unnecessary comment lines
    Signed-off-by: Arda Celik <ardacelik.dev@gmail.com>

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -34,42 +34,33 @@ const Chat = () => {
         <div className="flex flex-col h-screen  bg-indigo-300">
             <div className="flex-1 overflow-y-auto px-20 py-5">
 
-            {allMessages && allMessages.map((message, index) => (
+            {
+            allMessages && allMessages.map((message, index) => (
                 <div
-            key={index}
-            className={`flex flex-col ${
-              message.username === username ? "items-end" : "items-start"
-            }`}
-            >
-            <div
-              className={`rounded px-3 py-2 ${
-                message.username === username ? "bg-green-500" : "bg-blue-500"
-              } text-white`}
-            >
-              {message.message}
-            </div>
-            <div
-              className={`text-base ${
-                message.username === username
-                  ? "text-right text-green-700"
-                  : "text-left text-blue-700"
-              }`}
-            >
-              {message.username}
-            </div>
-          </div>
+                key={index}
+                className={`flex flex-col ${
+                message.username === username ? "items-end" : "items-start"
+                }`}
+                >
+                    <div
+                    className={`rounded px-3 py-2 ${
+                    message.username === username ? "bg-green-500" : "bg-blue-500"
+                    } text-white`}
+                    >
+                    {message.message}
+                    </div>
+                    <div
+                    className={`text-base ${
+                    message.username === username
+                        ? "text-right text-green-700"
+                        : "text-left text-blue-700"
+                    }`}
+                    >
+                    {message.username}
+                    </div>
+                </div>
             ))}
 
-
-                {/* <div className="w-5/12 h-[70px] bg-lime-500 gap-y-2 rounded-lg flex flex-col bg-lime">
-                    <div className="w-5/12 px-5 text-left text-2xl">
-                        <p>asdasdasdasddadasdas</p>
-                    </div>
-                    <div className="w-12/12 h-[25px] px-5 flex justify-between text-base bg-color-lime-500">
-                        <p>Ali Veli</p>
-                        <p>21:30</p>
-                    </div>
-                </div> */}
             </div>
             <div className="flex justify-center pb-5 px-20 gap-x-4">
                 <input value={message} onChange={(e) => setMessage(e.target.value)} type="text" className="w-9/12 h-9 rounded-md" />
